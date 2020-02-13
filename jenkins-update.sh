04edbf0cb25aea4e808406756d904a27b45cfb03
wallace.salles <wallace.salles@corp.globo.com>
2020-02-13 11:28:48 -0300

    Add Script to update Jenkins

---
+++ jenkins-update.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+# ----------------
+# Jenkins Patch
+patch=/usr/lib/jenkins/jenkins.war
+
+# Link External: Latest Version
+link_external=http://updates.jenkins-ci.org/latest/jenkins.war
+# ----------------
+
+## Steps:
+echo -e "::::::::::::::::::::\n:: Update Jenkins ::\n::::::::::::::::::::\n\n"
+# 1) Check Current Version
+version=$(grep -Po '(?<=version>)[^<]+' /var/lib/jenkins/config.xml)
+echo -e "Current Version: $version \n"
+
+# 2) Save old version:
+cp -f $patch $patch.$version
+echo -e "Save old patch: [ $patch.$version ] \n"
+
+# 3) Download Latest Version
+echo -e "Download Latest Version: $link_external\n"
+wget -O $patch $link_external
+
+# 4) Apply Update: Restart Jenkins
+echo -e "\nApply Update...\n"
+systemctl restart jenkins
+echo -e "Finish!"
