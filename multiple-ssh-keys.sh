9fdec134ba4ad55bb6e7a91cab100e6e7b4b112c
Wallace Salles <wallace_robinson@hotmail.com>
2021-07-13 10:41:12 -0300

    [multiple-ssh-keys] Improving the code

--- multiple-ssh-keys.sh
+++ multiple-ssh-keys.sh
@@ -1,30 +1,44 @@
 #!/usr/bin/env bash
+#-----------------------------------------------------------------------------------------------------------------------#
+#
+#   * Website    : https://wallacesalles.dev
+#   * Athor      : Wallace Salles
+#   * Mainteiner : Wallace Salles
+#
 # VARIABLES -------------------------------------------------------------------------------------------
-name["personal"]="wsalles"
-name["job"]="wallacesalles-hotmart"
+declare -A name
+declare -A email
+declare -A key
+
+#name["personal"]="wsalles"
+name["personal"]="Wallace Salles"
 email["personal"]="wallace_robinson@hotmail.com"
-email["job"]="wallace.salles@hotmart.com"
 key["personal"]="id_rsa_wallace_robinson@hotmail.com"
+
+name["job"]="wallacesalles-hotmart"
+email["job"]="wallace.salles@hotmart.com"
 key["job"]="id_rsa"
 
 type="$1"
+USAGE_MSG="Usage: source $0 <type> \nType: [personal, job]"
 
-# TESTS -----------------------------------------------------------------------------------------------
-if [[ "${type}" = "job"  ]] || [[ "${type}" == "personal" ]]; then
+# METHODS ---------------------------------------------------------------------------------------------
+function task {
+  echo -e "Let's the use:\n- ${name["$type"]}\n- ${email["$type"]}\n- ${key["$type"]}\n"
+  ssh-add -D
+  ssh-add ~/.ssh/${key["$type"]}
+  ssh-add -l
+
+  git config user.name ${name["$type"]}
+  git config user.email ${email["$type"]}
+}
+
+# TESTS AND RUN ---------------------------------------------------------------------------------------
+if [[ "${type}" == "job"  ]] || [[ "${type}" == "personal" ]]; then
   echo "Passed!"
+  task
 else
-  echo "Usage: $0 <type>" && echo "Type: [personal, job]" && exit 1;
+  echo -e ${USAGE_MSG};
 fi
 
-# RUN -------------------------------------------------------------------------------------------------
-
-echo "Vamos usar o nome ${name[$type]}, o email ${email[$type]} e a chave ${key[$type]}"
-
-ssh-add -D
-ssh-add ~/.ssh/${key[$type]}
-ssh-add -l
-
-git config user.name ${name[$type]}
-git config user.email ${email[$type]}
-
-
+# -----------------------------------------------------------------------------------------------------
