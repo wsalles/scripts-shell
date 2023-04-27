388b3479f02cf5bc9cf04a3eec3358c71f3d0d64
wsalles <wallace_robinson@hotmail.com>
2023-04-27 22:03:17 +0100

    feat: code refactoring

--- cleanup.sh
+++ cleanup.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
-
-root="/home"
+#-----------------------------------------------------------------------------------------------------------------------#
+#
+#   * Website    : https://wallacesalles.dev/
+#   * Author     : Wallace Salles
+#   * Mainteiner : Wallace Salles
+#
+#- VARIABLES -----------------------------------------------------------------------------------------------------------#
+path=(/home /tmp)
 days=10
-NOW=$(date '+%Y-%m-%d %H:%M:%S')
-LOG_FILE="/var/log/remove_GloboPlayAtemeTar.log"
+LOG_FILE="/var/log/my-awesome-app.log"
 
-# GloboPlayAtemeTar
-alias echo='echo $NOW' >> "$LOG_FILE"
-echo "Lista de arquivos deletados:" >> "$LOG_FILE"
+#- FUNCTIONS -----------------------------------------------------------------------------------------------------------#
+# Loading Default Functions
+. $(dirname $0)/functions/others
 
-for dst in \
-        ateme/GloboPlayAtemeTar; do
-        find ${root}/${dst} -type f -mtime +$days -print -exec rm -f "{}" \; >> "$LOG_FILE"
+#- Starting ------------------------------------------------------------------------------------------------------------#
+log "Lista de arquivos deletados:" >> "$LOG_FILE"
+
+for dst in $path; do
+  find ${dst} -type f -mtime +$days -print -exec rm -f "{}" \; >> "$LOG_FILE"
 done
